4caf2e26c2a54e4c863dd043617808a0eb5f1f0f
TheWolf81 <joaorodrigues2003@gmail.com>
2024-09-06 02:45:20 +0100

    test route (for connectivity test

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -7,6 +7,12 @@ export default ({app}: {app: express.Application}) => {
   app.use(express.json());
   app.use(express.urlencoded({extended: false}));
   app.use(cors());
+  //add test route
+  app.get('/test', (req, res) => {
+    //send code 200 and message 'Hello World'
+    res.status(200).send('Hello World');
+  }
+  );
   app.use(bodyParser.json());
   app.use(routes());
   return app;
